b2b6f75d7661f7dc61c09b790bdc49b82e2a60fa
feat630 <630@hansung.ac.kr>
2023-01-19 02:52:52 +0900

    test

--- server/server.js
+++ server/server.js
@@ -4,5 +4,5 @@ const routes = require('./Router/routes');
 
 app.use('/', routes);
 
-const port=4000; //React가 3000번 포트를 사용하기 때문에 node 서버가 사용할 포트넘버는 다른 넘버로 지정해준다.
+const port=4000;
 app.listen(port, ()=>{console.log(`Listening on port ${port}`)});
