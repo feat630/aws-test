cb03ef2d3c1973967df06361762fccbf5a68ad73
feat630 <630@hansung.ac.kr>
2023-02-10 12:04:44 +0900

    카카오맵 연동성공, api 키 .env파일로 연동 깃이그노어 추가

--- my-app/src/component/Random.jsx
+++ my-app/src/component/Random.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from "react"; 
+import React from "react"; 
+import { Map } from 'react-kakao-maps-sdk';
 // import { Link, useNavigate } from "react-router-dom";
 // import axios from "axios";
 
 export const Home = () => {
-    
-    const [name, setName] = useState();
 
     return (
-        <>
-            <button>random</button>
-        </>
+            <Map 
+                center={{ lat: 33.5563, lng: 126.79581 }}   // 지도의 중심 좌표
+                style={{ width: '800px', height: '600px' }} // 지도 크기
+                level={3}                                   // 지도 확대 레벨
+            >
+            </Map>
     )
 
 
