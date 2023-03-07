6895f4e742d73f7ca444c47155e4321bcdc8cdc8
feat630 <630@hansung.ac.kr>
2023-03-07 15:28:08 +0900

    로고추가 및 index파일 수정

--- my-app/src/component/TopMenu.jsx
+++ my-app/src/component/TopMenu.jsx
@@ -8,7 +8,7 @@ export const TopMenu = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="/">오늘뭐먹지?</Navbar.Brand>
+                <Navbar.Brand href="/"><img src="E:\Dev\aws-test\my-app\public\logo.png"/>오늘뭐먹지?</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
