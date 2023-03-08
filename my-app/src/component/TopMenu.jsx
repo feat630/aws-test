0244a129e8ff172c556e8f37e54ab59b383b745f
feat630 <630@hansung.ac.kr>
2023-03-09 02:04:50 +0900

    로그인 상태에 따라 topmenu 변경

--- my-app/src/component/TopMenu.jsx
+++ my-app/src/component/TopMenu.jsx
@@ -5,39 +5,77 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export const TopMenu = () => {
 
-    return (
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="/">
-                    오늘뭐먹지?
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="random">랜덤식당고르기</Nav.Link>
-                    <Nav.Link href="#link">리뷰보러가기</Nav.Link>
-                    <NavDropdown title="마이페이지" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">내가 쓴 리뷰보기</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                        개인정보 수정
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">로그아웃</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">
-                        OOO님 반갑습니다
-                    </NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                </Navbar.Collapse>
-                <Navbar.Collapse className="justify-content-end">
-                    <Navbar.Text>
-                        <a href="login">login</a>
-                    </Navbar.Text>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+    const isLogin = true;
 
+    if(isLogin) {
+        return (
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/">
+                        오늘뭐먹지?
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="random">랜덤식당고르기</Nav.Link>
+                        <Nav.Link href="#link">리뷰보러가기</Nav.Link>
+                        <NavDropdown title="마이페이지" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">내가 쓴 리뷰보기</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">
+                            개인정보 수정
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">로그아웃</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">
+                            로그인이 필요합니다
+                        </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    </Navbar.Collapse>
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text>
+                            <a href="login">login</a>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )    
+    } else {
+        return (
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/">
+                        오늘뭐먹지?
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="random">랜덤식당고르기</Nav.Link>
+                        <Nav.Link href="#link">리뷰보러가기</Nav.Link>
+                        <NavDropdown title="마이페이지" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">내가 쓴 리뷰보기</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">
+                            개인정보 수정
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">로그아웃</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">
+                            OOO님 반갑습니다
+                        </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    </Navbar.Collapse>
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text>
+                            <a href="login">logout</a>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )    
+    }
+
+    
 
 }
 
